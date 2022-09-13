0966f117129d8973a83bc86de40db34c831db1fc
Daniel OGIDAN <hoghidan1@gmail.com>
2022-09-13 10:17:28 +0100

    Update COVID Portfolio Project - Data Exploration.sql

--- COVID Portfolio Project - Data Exploration.sql
+++ COVID Portfolio Project - Data Exploration.sql
@@ -1,3 +1,10 @@
+
+
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 /*
 Kudos to AlexTheAnalyst###
 
@@ -7,10 +14,48 @@ i import coviddeaths and covidvaccination .csv file that was saved in the postgr
 
 */
 
+COPY coviddeaths FROM 'C:\Program Files\PostgreSQL\14\titan\coviddeaths.csv'
+DELIMITERS ','  CSV HEADER;  (sample....to import to postgres dmbs)
+
 /*
-Covid 19 Data Exploration 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Selecting where conitinent is not null
+Selecting where all the people vaccinated
+
+order operations
 */
+
+Select *
+From coviddeaths
+Where continent is not null 
+order by continent DESC
+
+Select *
+From coviddeaths
+Where people_fully_vaccinated is not null 
+order by continent DESC
+
+
+/*
+
+-- Select Data that we are going to be starting with
+*/
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From coviddeaths
+Where continent is not null 
+order by DESC
+
+-- Total Cases vs Total Deaths
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From coviddeaths
+Where location like '%states%'
+and continent is not null 
+order by DESC
+
+-- Shows likelihood of dying if you contract covid in your country
+
+
 
 
 
